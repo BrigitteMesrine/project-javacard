bae0205f18e33ede8339de9e9f554e755b84118c
KeoVincent <keokouadio@gmail.com>
2024-08-02 11:30:14 +0200

    somehow working as it is, overwrites correctly

--- src/main/java/fr/afpa/Test.java
+++ src/main/java/fr/afpa/Test.java
@@ -14,11 +14,14 @@ public class Test {
         "36 rue du trente-six", "33360", Contact.Gender.MALE, null, null, null, null);
         Contact contactTest2 = new Contact("Jean", "JEAN", "0707070707", "jean@jean.com",
         "37 rue du trente-sept", "33370", Contact.Gender.NON_BINARY, LocalDate.of(1985, 10, 26), null, null, null);
+        Contact contactTest3 = new Contact("Pierre", "PIERRE", "0708080808", "pierre@caillou.com",
+        "38 rue du trente-huit", "33380", Contact.Gender.FEMALE, LocalDate.of(1985, 10, 26), null, null, null);
 
         // add new contacts to this list, modify existing objects from this list
         ArrayList<Contact> contacts = new ArrayList<>();
         contacts.add(contactTest);
         contacts.add(contactTest2);
+        contacts.add(contactTest3);
 
         // then overwrite serial file with updated ArrayList<>
         ContactBinarySerializer contactBinarySerializer = new ContactBinarySerializer(); // is there a way to make this static ?
